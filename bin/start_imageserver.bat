9818136b62dc03ad8666767a4c19063baedb1337
Michael Lesser <mlesser@arizona.edu>
2022-12-02 08:26:41 -0700

    cleanup

--- bin/start_imageserver.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem Starts AzCamImageWriter under Windows.
-
-set ROOT="/azcam/azcam-imageserver/azcam_imageserver"
-start /high /min /d %ROOT% "AzCamImageServer" python -m imageserver -l 6543 -v
